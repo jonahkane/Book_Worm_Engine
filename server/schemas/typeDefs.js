3af4564bd4c2a1ca7eb724643ca99ccc418024d7
Jonah <jonahtkane@gmail.com>
2023-05-16 22:44:36 -0500

    updated typedefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -16,35 +16,18 @@ type User {
     email: String!
     bookCount: String!
 }
+
+type Query {
+    book: [Book]
+    user: [User]
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+
+}
 `
-
-// const typeDefs = gql`
-//   type Tech {
-//     _id: ID!
-//     name: String!
-//   }
-
-//   type Matchup {
-//     _id: ID!
-//     tech1: String!
-//     tech2: String!
-//     tech1_votes: Int
-//     tech2_votes: Int
-//   }
-
-//   type Query {
-//     tech: [Tech]
-//     matchups(_id: String): [Matchup]
-//   }
-
-//   type Mutation {
-//     createMatchup(tech1: String!, tech2: String!): Matchup
-//     createVote(_id: String!, techNum: Int!): Matchup
-//   }
-// `;
-
-// module.exports = typeDefs;
-
 
 
 // // typeDefs.js: Define the necessary Query and Mutation types:
@@ -54,9 +37,7 @@ type User {
 // // me: Which returns a User type.
 // // Mutation type:
 
-// // login: Accepts an email and password as parameters; returns an Auth type.
 
-// // addUser: Accepts a username, email, and password as parameters; returns an Auth type.
 
 // // saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
 
