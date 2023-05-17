312f646849520a0569903b0ded9189e672859bd6
Jonah <jonahtkane@gmail.com>
2023-05-16 22:21:27 -0500

    added sample code to typedefs.js NEED TO UPDATE

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,46 +1,75 @@
-// typeDefs.js: Define the necessary Query and Mutation types:
+// // typeDefs.js: Define the necessary Query and Mutation types:
 
-// Query type:
+// // Query type:
 
-// me: Which returns a User type.
-// Mutation type:
+// // me: Which returns a User type.
+// // Mutation type:
 
-// login: Accepts an email and password as parameters; returns an Auth type.
+// // login: Accepts an email and password as parameters; returns an Auth type.
 
-// addUser: Accepts a username, email, and password as parameters; returns an Auth type.
+// // addUser: Accepts a username, email, and password as parameters; returns an Auth type.
 
-// saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
+// // saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
 
-// removeBook: Accepts a book's bookId as a parameter; returns a User type.
+// // removeBook: Accepts a book's bookId as a parameter; returns a User type.
 
-// User type:
+// // User type:
 
-// _id
+// // _id
 
-// username
+// // username
 
-// email
+// // email
 
-// bookCount
+// // bookCount
 
-// savedBooks (This will be an array of the Book type.)
+// // savedBooks (This will be an array of the Book type.)
 
-// Book type:
+// // Book type:
 
-// bookId (Not the _id, but the book's id value returned from Google's Book API.)
+// // bookId (Not the _id, but the book's id value returned from Google's Book API.)
 
-// authors (An array of strings, as there may be more than one author.)
+// // authors (An array of strings, as there may be more than one author.)
 
-// description
+// // description
 
-// title
+// // title
 
-// image
+// // image
 
-// link
+// // link
 
-// Auth type:
+// // Auth type:
 
-// token
+// // token
 
-// user (References the User type.)
+// // user (References the User type.)
+
+// const { gql } = require('apollo-server-express');
+
+// const typeDefs = gql`
+//   type Tech {
+//     _id: ID!
+//     name: String!
+//   }
+
+//   type Matchup {
+//     _id: ID!
+//     tech1: String!
+//     tech2: String!
+//     tech1_votes: Int
+//     tech2_votes: Int
+//   }
+
+//   type Query {
+//     tech: [Tech]
+//     matchups(_id: String): [Matchup]
+//   }
+
+//   type Mutation {
+//     createMatchup(tech1: String!, tech2: String!): Matchup
+//     createVote(_id: String!, techNum: Int!): Matchup
+//   }
+// `;
+
+// module.exports = typeDefs;
