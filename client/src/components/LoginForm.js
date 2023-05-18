d9cf79c52798490517a930736611b252be3e4127
Jonah <jonahtkane@gmail.com>
2023-05-17 22:23:05 -0500

    updatd login form js loginuser function

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -9,7 +9,7 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [login, { error, data }] = useMutation(LOGIN_USER);
+  const [login, { error }] = useMutation(LOGIN_USER);
 
 
   const handleInputChange = (event) => {
@@ -28,15 +28,21 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const { data } = await login({ variables: { ...userFormData } 
+      });
+      console.log(data);
+      Auth.login(data.login.token);
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      // const response = await loginUser(userFormData);
+
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
+
+      // const { token, user } = await response.json();
+      // console.log(user);
+      // Auth.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
