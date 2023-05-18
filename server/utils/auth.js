c52d6d212a29b9e6f2ddbe01d9043c10651cf5ae
Jonah <jonahtkane@gmail.com>
2023-05-17 20:59:28 -0500

    added code for user auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -37,3 +37,16 @@ module.exports = {
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
+
+//code below from jwt activity 21 - rework...
+// const jwt = require('jsonwebtoken');
+
+// const secret = 'mysecretssshhhhhhh';
+// const expiration = '2h';
+
+// module.exports = {
+//   signToken: function ({ email, name, _id }) {
+//     const payload = { email, name, _id };
+//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+//   },
+// };
