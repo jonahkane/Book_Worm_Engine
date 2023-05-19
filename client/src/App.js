5c5a2b5e97d73243102d01546f5875ab481ccb9d
Jonah <jonahtkane@gmail.com>
2023-05-19 02:09:40 -0500

    debugging. re did package json to fix react router dom issue

--- client/src/App.js
+++ client/src/App.js
@@ -33,9 +33,18 @@ function App() {
       <>
         <Navbar />
         <Routes>
-          <Route path='/' element={<SearchBooks/>} />
-          <Route path='/saved' element={<SavedBooks/>} />
-          <Route path="*" element={<h1 className='display-2'>Wrong page!</h1>} />
+          <Route 
+          path='/' 
+          element={<SearchBooks />} 
+          />
+          <Route 
+          path='/saved' 
+          element={<SavedBooks />} 
+          />
+          <Route
+          path='*'
+          element={<h1 className='display-2'>Wrong page!</h1>} 
+          />
         </Routes>
       </>
     </Router>
