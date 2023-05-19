5597de1c7adfc7eb12d2c5e5a6cd76a81ed2b2bf
Jonah <jonahtkane@gmail.com>
2023-05-19 02:25:15 -0500

    i dont even know what i changed this time

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -11,8 +11,8 @@ import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
 const { loading, data } = useQuery(QUERY_SINGLEUSER)
-const userData = data?.getSingleUser || []
-console.log(userData);
+
+
 
 const [removeBook] = useMutation(DELETE_BOOK, {
   updated(cache, { data: removeBook }) {
@@ -26,14 +26,8 @@ const [removeBook] = useMutation(DELETE_BOOK, {
     }
   }
 })
+const userData = data?.getSingleUser || []
 
-  // const [userData, setUserData] = useState({});
-
-  // const userDataLength = Object.keys(userData).length;
-
-  // if (!userData?.username) {
-  //   return <h4>Log in to view this page.</h4>;
-  // }
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -41,27 +35,18 @@ const [removeBook] = useMutation(DELETE_BOOK, {
       return false;
     }
 
+    removeBookId(bookId);
+
+
     try {
       await removeBook({
         variables: { bookId }
       });
-      console.log(userData);
-      removeBookId(bookId);
-
-      // const response = await deleteBook(bookId, token);
-
-      // if (!response.ok) {
-      //   throw new Error("something went wrong!");
-      // }
-
-      // const updatedUser = await response.json();
-      // setUserData(updatedUser);
-      // // upon success, remove book's id from localStorage
-      // removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
+    
 
   // if data isn't here yet, say so
   if (loading) {
